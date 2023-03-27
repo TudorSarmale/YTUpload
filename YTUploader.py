962abda398eb88a5e192e1af2218524326b6d4b9
Voie Tudor <75695432+TudorSarmale@users.noreply.github.com>
2023-03-27 13:20:52 +0300

    remove useless stuff

--- YTUploader.py
+++ YTUploader.py
@@ -5,18 +5,14 @@ import threading
 
 threads = []
 
-def uploadCaller(filename):
-	print ("Starting upload for file " + filename)
+def up(filename):
 	os.system("python3 " + sys.argv[1] + " --file=" + filename + " --title=" + filename + " --privacyStatus=" + sys.argv[3])
 
 os.chdir(sys.argv[2])
 for file in glob.glob("*.*"):
-	t = threading.Thread(target=uploadCaller(file))
+	t = threading.Thread(target=up(file))
 	threads.append(t)
 	t.start()
 
-
 for thread in threads:
 	thread.join()
-	
-print ("Done!")
